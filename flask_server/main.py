4fa2254dd5938621ba21e4b91a00e8767166fffe
lym0909 <pgwy9990@gmail.com>
2016-11-05 19:44:24 +0900

    동일 메소드 수정

--- flask_server/main.py
+++ flask_server/main.py
@@ -12,25 +12,17 @@ from flask import url_for
 from translate import translate_ms
 
 app = Flask(__name__)
-
-@app.route('/translate', methods=['POST'])
-def mainMethod():
-    if request.method == 'POST':
-        original = request.form['original']
-        from_lang = request.form['from']
-        to_lang = request.form['to']
     
 @app.route('/translate')
-def mainMethod():
-        
+def returnMainTemplate():
     return render_template('translateSite.html')
 
 @app.route('/translate/<langFrom>/<langTo>/<senten>', methods=['GET'])
-def testMethod(langFrom, langTo, senten):
+def returnTransFromFullURL(langFrom, langTo, senten):
     return translate_ms(senten, langFrom, langTo) #return json
 
 @app.route('/translate', methods=['POST'])
-def returnTrans():
+def returnTransFromPOST():
     if request.method == 'POST':
         from_lang = request.form['from_lang']
         to_lang = request.form['to_lang']
